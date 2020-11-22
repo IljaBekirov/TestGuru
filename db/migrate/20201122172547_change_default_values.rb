c3525af173f48d6ca8dfc09b33c04a7b63e2a111
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-11-23 01:33:40 +0300

    Edit migrations default answer

--- db/migrate/20201122172547_change_default_values.rb
+++ db/migrate/20201122172547_change_default_values.rb
@@ -1,6 +1,5 @@
 class ChangeDefaultValues < ActiveRecord::Migration[6.0]
   def change
     change_column_default :tests, :level, from: nil, to: 0
-    change_column_default :answers, :correct, from: false, to: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_11_22_221930) do
 
   create_table "answers", force: :cascade do |t|
-    t.boolean "correct", default: true, null: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
