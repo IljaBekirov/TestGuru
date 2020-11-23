4217b0c6bc30c3a70b82821164e2626116e653b7
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-11-24 00:21:38 +0300

    default models in seeds.rb and add instance method and method class

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Test < ApplicationRecord
+  has_and_belongs_to_many :users
+  belongs_to :category
+
+  def self.test(category)
+    joins(:category).where("categories.title = ?", category).order(title: :desc).pluck(:title)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories_title = ['Backend', 'Frontend', 'Mobile Development', 'Machine Learning']
+categories = categories_title.map { |category| Category.create!([{ title: category }]) }.flatten
+
+tests = Test.create!([
+                       { title: 'Ruby on Rails', level: 1, category_id: categories[0].id },
+                       { title: 'Html', level: 0, category_id: categories[1].id },
+                       { title: 'Python', level: 2, category_id: categories[3].id },
+                       { title: 'C++', level: 3, category_id: categories[0].id }
+                     ])
+
+questions = Question.create!([
+                               { body: 'Question 1 ?', test_id: tests[0].id },
+                               { body: 'Question 2 ?', test_id: tests[1].id },
+                               { body: 'Question 5 ?', test_id: tests[2].id },
+                               { body: 'Question 3 ?', test_id: tests[3].id },
+                               { body: 'Question 4 ?', test_id: tests[0].id }
+                             ])
+
+Answer.create!([
+                 { body: 'Answer 1', correct: true, question_id: questions[0].id },
+                 { body: 'Answer 2', correct: true, question_id: questions[1].id },
+                 { body: 'Answer 3', correct: false, question_id: questions[4].id },
+                 { body: 'Answer 4', correct: true, question_id: questions[3].id },
+                 { body: 'Answer 5', correct: false, question_id: questions[2].id },
+                 { body: 'Answer 6', correct: true, question_id: questions[0].id },
+                 { body: 'Answer 7', correct: false, question_id: questions[1].id }
+               ])
+
+users_name = %w[User_1 User_2 User_3]
+
+users_name.map { |name| User.create!([{ name: name }]) }
