a9800e8f9634adad84087678278837edf6ca795a
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-11-25 00:06:09 +0300

    Revert "Add ER-diagram and add associations between models"
    This reverts commit 988b09b0

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@
 
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
-  has_many :created_tests, class_name: 'Test', foreign_key: 'user_id'
 
   def tests_list(level)
     tests.where(level: level)
