b99437d8d5e36a78385dffc92a92f134ee7bdbfe
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-11-26 20:32:47 +0300

    Add validates and scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,16 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
 
+  validates :title, presence: true
+  validates :level, presence: true, numericality: { only_integer: true }
+  validates :title, uniqueness: { scope: :level, message: 'Should uniq level and title' }
+
+  scope :easy_level, -> { where(level: 0..1) }
+  scope :medium_level, -> { where(level: 2..4) }
+  scope :hard_level, -> { where(level: 5..Float::INFINITY) }
+  scope :category_fo_test, ->(category_title) { joins(:category).where('categories.title = ?', category_title) }
+
   def self.test(category)
-    joins(:category).where('categories.title = ?', category).order(title: :desc).pluck(:title)
+    category_fo_test(category).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :created_tests, class_name: 'Test', foreign_key: 'user_id'
 
+  validates :name, presence: true
+
   def tests_list(level)
     tests.where(level: level)
   end
