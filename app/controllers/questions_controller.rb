e60e4a32c51ed0bbff50750bd9c945af5c7e2497
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-11-27 22:36:21 +0300

    Add controllers questions and test

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class QuestionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+
+  def index
+    questions = @test.questions
+
+    render inline: questions.map(&:body).join('<br> ')
+  end
+
+  def show
+    render inline: @question.body
+  end
+
+  def new; end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to test_path(@test), notice: 'Вопрос создан'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to test_path(@question.test_id), notice: 'Вопрос удален'
+  end
+
+  private
+
+  def rescue_with_test_not_found
+    render plain: 'Вопрос не найден'
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+end
