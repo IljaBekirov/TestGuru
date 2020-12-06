3f0116c7e09b4ee4da1ef7c310bdd0cb3bf4ac12
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-12-06 23:03:34 +0300

    fix migrations delete migration with name

--- db/migrate/20201204234314_delete_name_from_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DeleteNameFromUsers < ActiveRecord::Migration[6.0]
-  def up
-    remove_column :users, :name, if_exists: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -80,7 +80,6 @@ ActiveRecord::Schema.define(version: 20_201_206_143_858) do
     t.string 'first_name'
     t.string 'last_name'
     t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
     t.index ['type'], name: 'index_users_on_type'
   end
