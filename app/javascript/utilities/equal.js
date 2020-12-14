7b0e26bc6677a489127af8be76115695401b166f
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-12-15 01:52:38 +0300

    fix when clear input

--- app/javascript/utilities/equal.js
+++ app/javascript/utilities/equal.js
@@ -3,30 +3,47 @@ document.addEventListener('turbolinks:load', function () {
   var confirm_pass = document.getElementById('confirm_pass')
 
   pass.oninput = function () {
-    if (pass.value === confirm_pass.value) {
-      pass.classList.add('is-valid')
-      pass.classList.remove('is-invalid')
-      confirm_pass.classList.add('is-valid')
-      confirm_pass.classList.remove('is-invalid')
+    if (pass.value !== '') {
+      if (pass.value === confirm_pass.value) {
+        addValidClass(pass, confirm_pass)
+      } else {
+        addInValidClass(pass, confirm_pass)
+      }
     } else {
-      pass.classList.add('is-invalid')
-      pass.classList.remove('is-valid')
-      confirm_pass.classList.add('is-invalid')
-      confirm_pass.classList.remove('is-valid')
+      clearClass(pass, confirm_pass)
     }
   }
 
   confirm_pass.oninput = function () {
-    if (pass.value === confirm_pass.value) {
-      pass.classList.add('is-valid')
-      pass.classList.remove('is-invalid')
-      confirm_pass.classList.add('is-valid')
-      confirm_pass.classList.remove('is-invalid')
+    if (confirm_pass.value !== '') {
+      if (pass.value === confirm_pass.value) {
+        addValidClass(pass, confirm_pass)
+      } else {
+        addInValidClass(pass, confirm_pass)
+      }
     } else {
-      pass.classList.add('is-invalid')
-      pass.classList.remove('is-valid')
-      confirm_pass.classList.add('is-invalid')
-      confirm_pass.classList.remove('is-valid')
+      clearClass(pass, confirm_pass)
     }
   }
 })
+
+function clearClass(pass, confirm_pass) {
+  confirm_pass.classList.remove('is-valid')
+  confirm_pass.classList.remove('is-invalid')
+  pass.classList.remove('is-valid')
+  pass.classList.remove('is-invalid')
+}
+
+function addValidClass(pass, confirm_pass) {
+  pass.classList.add('is-valid')
+  pass.classList.remove('is-invalid')
+  confirm_pass.classList.add('is-valid')
+  confirm_pass.classList.remove('is-invalid')
+}
+
+function addInValidClass(pass, confirm_pass) {
+  pass.classList.add('is-invalid')
+  pass.classList.remove('is-valid')
+  confirm_pass.classList.add('is-invalid')
+  confirm_pass.classList.remove('is-valid')
+}
