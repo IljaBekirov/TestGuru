befcd658fb9744571e010bc6b1d552baa49e5178
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-12-18 00:05:19 +0300

    Edit production.rb for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'sheltered-plains-94277.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
+    domain: 'sheltered-plains-94277.herokuapp.com',
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
