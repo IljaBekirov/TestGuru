f5aa5626b0c6f9db8636c11e8e2b001782932745
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-12-17 22:45:07 +0300

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
 
 module TestGuru
   class Application < Rails::Application
@@ -21,9 +22,5 @@ module TestGuru
     # the framework and any gems in your application.
 
     config.autoload_paths << "#{Rails.root}/lib/clients"
-
-    Bundler.require(*Rails.groups)
-    Dotenv::Railtie.load
-    HOSTNAME = ENV['HOSTNAME']
   end
 end
