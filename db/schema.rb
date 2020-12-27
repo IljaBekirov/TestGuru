04130c3e77e7f37686d4577884a94180ef91d182
Ilya Bekirov <i.bekirov@parts-soft.ru>
2020-12-27 15:07:45 +0300

    Add timer for test

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_223_195_930) do
+ActiveRecord::Schema.define(version: 20_201_226_205_447) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20_201_223_195_930) do
     t.datetime 'updated_at', precision: 6, null: false
     t.integer 'category_id'
     t.bigint 'user_id'
+    t.integer 'timer'
     t.index ['category_id'], name: 'index_tests_on_category_id'
     t.index %w[level title], name: 'index_tests_on_level_and_title', unique: true
     t.index ['user_id'], name: 'index_tests_on_user_id'
